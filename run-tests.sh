fc6eabd2e2f32122051af20c7a7d00e56f43ec7f
Barry Flinn <bflinn@connectrn.com>
2022-10-04 09:49:13 -0400

    pg test fixes, maybe?

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
 sh scripts/database/support/init-db.sh connectrn_test
-PGPASSWORD=canary psql -U connectrn connectrn_test -f scripts/database/sql/table-scripts.sql
+PGPASSWORD=canary psql -h postgres -U connectrn connectrn_test -f scripts/database/sql/table-scripts.sql
 go clean -testcache
-DB_HOST=localhost DB_NAME=connectrn_test TESTDATA_PATH=$(pwd)/testdata gotestsum ./...
+DB_HOST=postgres DB_NAME=connectrn_test TESTDATA_PATH=$(pwd)/testdata gotestsum ./...
